19aa351791610803a65756d7452a5ec026b5fd4b
jabelic <37907624+jabelic@users.noreply.github.com>
2025-03-08 11:31:34 +0900

    core export node type (#35)
    * feat: 🎸 editor overflow scroll
    
    * feat: 🎸 export NodeType from core packages

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-// export { Speech, ProsodyOptions } from "./interfaces/ssml-tags";
-export { DAGNode, SSMLDAG } from "./implementations/dag";
+export { DAGNode, SSMLDAG, NodeType } from "./implementations/dag";
 export { Result, success, failure } from "./implementations/result";
 export { parseSSML } from "./implementations/parser/index";
